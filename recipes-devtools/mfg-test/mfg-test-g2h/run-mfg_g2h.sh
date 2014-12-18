d2e26f4f0fc81e00385ff23274ad9ba9e0904589
Stephano Cetola <scetola@reachtech.com>
2014-12-18 13:32:35 -0800

    update run mfg script for new hawthorne boards
    Signed-off-by: Stephano Cetola <scetola@reachtech.com>

--- recipes-devtools/mfg-test/mfg-test-g2h/run-mfg_g2h.sh
+++ recipes-devtools/mfg-test/mfg-test-g2h/run-mfg_g2h.sh
@@ -4,7 +4,7 @@
 #
 MAC_1=30688C
 
-TESTS="LCD"
+TESTS="Ethernet,USB1,USB2,LCD,Backlight,AUART"
 SD_MNT_DIR=/tmp/sdcard
 MTD_NUM=1
 
@@ -19,37 +19,8 @@ read MAC_2
 /usr/sbin/mfg-test --mac-address=${MAC_1}${MAC_2}
 echo ""
 
-while true; do
-    read -p "Do you wish to copy the SD Card to NAND? [Type Y or N]: " yn
-    case $yn in
-        [Yy]* ) mkdir -p /images; 
-				mount /dev/mmcblk0p3 /images; 
-				flash_install.sh; 
-				
-				# copy over pointer cal
-				echo "Copying calibration file to NAND"
-				mkdir -p $SD_MNT_DIR
-				if [ -c /dev/ubi0_0 ]; then
-					/usr/sbin/ubidetach /dev/ubi_ctrl -m $MTD_NUM
-				fi
-				/usr/sbin/ubiattach /dev/ubi_ctrl -d 0 -m $MTD_NUM
-				sleep 3
-				mount -t ubifs /dev/ubi0_0 $SD_MNT_DIR
-				cp /etc/pointercal $SD_MNT_DIR/etc/
-				sync
-				umount $SD_MNT_DIR
-				/usr/sbin/ubidetach /dev/ubi_ctrl -m $MTD_NUM
-				sleep 3
-				rmdir $SD_MNT_DIR
-				break;;
-				
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
 if [ -f /etc/udev/rules.d/70-persistent-net.rules ]; then
-	rm /etc/udev/rules.d/70-persistent-net.rules
+        rm /etc/udev/rules.d/70-persistent-net.rules
 fi
 
 sync && sync

2e92802c4615ab25318ae9d25d15cc7459c9d03d
Stephano Cetola <scetola@reachtech.com>
2015-02-23 11:12:16 -0800

    update busybox with resize and rdev
    Brought over from dylan branch:
    Resize is handy if you want to edit files in a terminal window larger
    than 80 columns wide.  rdev is nice if you want to check which device
    you are booting from (block device).
    
    Signed-off-by: Stephano Cetola <scetola@reachtech.com>

--- recipes-support/reach-version/reach-version/version.sh
+++ recipes-support/reach-version/reach-version/version.sh
@@ -3,5 +3,9 @@
 export TMPDIR=/mnt/.psplash
 
 SN=$(cat /etc/reach-version  |grep "^meta-reach"|awk '{print $3}' | cut -c1-8)
+
+if [ "$(rdev)" = "" ]; then
+    SN=${SN}-NAND  
+fi
                                                                               
 /usr/bin/psplash-write "MSG $SN"

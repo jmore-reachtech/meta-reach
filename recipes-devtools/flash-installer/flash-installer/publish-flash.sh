6a74f522955133dd064fc9386c75e8cceb1d7cf1
Stephano Cetola <scetola@reachtech.com>
2015-07-29 15:59:35 -0700

    check install before publishing flash
    Signed-off-by: Stephano Cetola <scetola@reachtech.com>

--- recipes-devtools/flash-installer/flash-installer/publish-flash.sh
+++ recipes-devtools/flash-installer/flash-installer/publish-flash.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 echo "################################"
-echo "Publishing QML Source"
+echo "Publishing Source"
 echo "################################"
 mkdir /nand/
 /usr/sbin/ubiattach /dev/ubi_ctrl -d 0 -m 1
 sleep 1
 mount -t ubifs ubi0 /nand/
+sleep 1
+
+if [ -d "/Crank" ] && [ -d "/nand/Crank" ]; then
+  echo "SRC found for Crank Software"
+elif [ ! -d "/Crank" ] && [ ! -d "/nand/Crank" ]; then
+  echo "SRC found for QML"
+else
+  echo "##############################################"
+  echo "SRC is different on NAND. Restore NAND first."
+  echo "##############################################"
+  return 1
+fi
+
 rm -rf /nand/application/src/* && sync
 cp -rf /application/src/* /nand/application/src/ && sync
 umount /nand/

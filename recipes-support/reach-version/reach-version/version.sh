bc9bf6a73bc43df76516e11014a2eb721d94be5e
Jeff Horn <outforaride@gmail.com>
2017-03-27 15:52:24 -0700

    reach-version: Add check for PN in /etc/reach-pn
    The Reach Part Number has moved from /etc/reach-version to
    /etc/reach-pn. This update supports both locations by first checking for
    reach-pn and falling back to reach-version.
    
    Signed-off-by: Jeff Horn <outforaride@gmail.com>
    Signed-off-by: Fabio Berton <fabio.berton@ossystems.com.br>

--- recipes-support/reach-version/reach-version/version.sh
+++ recipes-support/reach-version/reach-version/version.sh
@@ -2,7 +2,14 @@
 
 export TMPDIR=/mnt/.psplash
 
-SN=$(cat /etc/reach-version  |grep "^meta-reach"|awk '{print $3}' | cut -c1-8)
+# set a default PN
+SN=S000-000
+
+if [ -f /etc/reach-pn ]; then
+	SN=$(cat /etc/reach-pn)
+elif [ -f /etc/reach-version ]; then
+	SN=$(cat /etc/reach-version  |grep "^meta-reach"|awk '{print $3}' | cut -c1-8)
+fi
 
 if [ "$(rdev)" = "" ]; then
     SN=${SN}-NAND  

7a9325c7a3e9a7a57809c756a75eeeb1965e8577
Jeff Horn <outforaride@gmail.com>
2017-04-03 14:54:05 -0700

    initscripts-1.0: Fix lcd-kill script
    The base IO and full IO boards have different fb numbers. This update
    checks for the correct fb so the fb can be blanked.
    
    Signed-off-by: Jeff Horn <outforaride@gmail.com>

--- recipes-core/initscripts/initscripts-1.0/lcd-kill.sh
+++ recipes-core/initscripts/initscripts-1.0/lcd-kill.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# base IO and full IO have different device numbers
+if [ -d /sys/devices/soc0/fb.24 ]; then
+	BLANK=/sys/devices/soc0/fb.24/graphics/fb0/blank
+elif [ -d /sys/devices/soc0/fb.26 ]; then
+    BLANK=/sys/devices/soc0/fb.26/graphics/fb0/blank
+else
+    BLANK=/dev/null
+fi
+
 case "$1" in
 start)
         # show this during quiet boot too.
@@ -7,7 +16,7 @@ start)
         ;;
 stop)
         echo "stoping the LCD panel..."
-        echo 1 > /sys/devices/soc0/fb.25/graphics/fb0/blank
+        echo 1 > ${BLANK}
         ;;
 *)
         echo "Usage: /etc/init.d/lcd{start|stop}" >&2

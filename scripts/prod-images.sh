d445ab3229aa8039bed2dc445d110918d4f8e14c
Jeff Horn <outforaride@gmail.com>
2017-12-14 13:04:04 -0800

    prod-images.sh: Prepare SW Part numbers for next release
    Signed-off-by: Jeff Horn <outforaride@gmail.com>

--- scripts/prod-images.sh
+++ scripts/prod-images.sh
@@ -13,34 +13,34 @@ function build_mfg() {
     M=$1
     P=$2
     MACHINE=$M REACH_PN=$P bitbake reach-version -c cleansstate
-    MACHINE=$M REACH_PN=$P bitbake reach-image-qt5
+    MACHINE=$M REACH_PN=$P bitbake u-boot-g2h-nor reach-image-qt5
     cp -L ${DEPLOY_DIR}/${M}/${IMG_PREFIX}${M}${IMG_EXT} ${P}.img
     zip ${P}.img.zip ${P}.img
 }
 
-rm -rf *.img
-rm -rf *.zip
+rm -f *.img
+rm -f *.zip
 
 # this matches the list below
 IMG_COUNT=16
 
 #         Machine name      Part number
-build_mfg "g2h-solo-3"      "S016-005"
-build_mfg "g2h-solo-3-r"    "S054-001"
-build_mfg "g2h-solo-3f"     "S058-001"
-build_mfg "g2h-solo-4"      "S017-005"
-build_mfg "g2h-solo-4f"     "S057-001"
-build_mfg "g2h-solo-13"     "S033-005"
-build_mfg "g2h-solo-13f"    "S059-001"
-build_mfg "g2h-solo-14"     "S034-005"
-build_mfg "g2h-solo-14f"    "S060-001"
-build_mfg "g2h-solo-11f"    "S022-005"
-build_mfg "g2h-solo-11f-r"  "S061-001"
-build_mfg "g2h-solo-12f"    "S023-005"
-build_mfg "g2h-solo-18f"    "S055-002"
+build_mfg "g2h-solo-3"      "S016-006"
+build_mfg "g2h-solo-3-r"    "S054-002"
+build_mfg "g2h-solo-3f"     "S058-002"
+build_mfg "g2h-solo-4"      "S017-006"
+build_mfg "g2h-solo-4f"     "S057-002"
+build_mfg "g2h-solo-6"      "S046-003"
+build_mfg "g2h-solo-7f"     "S063-002"
+build_mfg "g2h-solo-11f"    "S022-006"
+build_mfg "g2h-solo-11f-r"  "S061-002"
+build_mfg "g2h-solo-12f"    "S023-006"
+build_mfg "g2h-solo-13"     "S033-006"
+build_mfg "g2h-solo-13f"    "S059-002"
+build_mfg "g2h-solo-14"     "S034-006"
+build_mfg "g2h-solo-14f"    "S060-002"
+build_mfg "g2h-solo-18f"    "S055-003"
 build_mfg "g2h-solo-19f"    "S0XX-000"
-build_mfg "g2h-solo-6"      "S046-002"
-build_mfg "g2h-solo-7f"     "S063-001"
 
 IMG_BUILT=$(ls -lt *.zip | wc -l)
 

b3a80558b331b42c65e4309664c659f8b9e59f93
Jeff Horn <outforaride@gmail.com>
2017-12-21 10:20:36 -0800

    mfg-images.sh: RC2 release
    Signed-off-by: Jeff Horn <outforaride@gmail.com>

--- scripts/mfg-images.sh
+++ scripts/mfg-images.sh
@@ -25,22 +25,22 @@ rm -rf *.zip
 IMG_COUNT=16
 
 #         Machine name      Part number
-build_mfg "g2h-solo-3"      "51-0406-211"
-build_mfg "g2h-solo-3-r"    "61-0406-11"
-build_mfg "g2h-solo-3f"     "51-0406-411"
-build_mfg "g2h-solo-4"      "51-0406-201"
-build_mfg "g2h-solo-4f"     "51-0406-401"
-build_mfg "g2h-solo-13"     "51-0406-181"
-build_mfg "g2h-solo-13f"    "51-0406-081"
-build_mfg "g2h-solo-14"     "51-0406-191ES"
-build_mfg "g2h-solo-14f"    "51-0406-091"
-build_mfg "g2h-solo-11f"    "51-0406-051"
-build_mfg "g2h-solo-11f-r"  "61-0406-41"
-build_mfg "g2h-solo-12f"    "51-0406-42"
-build_mfg "g2h-solo-18f"    "51-0406-0D2"
-build_mfg "g2h-solo-19f"    "51-0406-0C1"
-build_mfg "g2h-solo-6"      "999-00100"
-build_mfg "g2h-solo-7f"     "61-0406-361ES1"
+build_mfg "g2h-solo-3"      "51-0406-211-RC2"
+build_mfg "g2h-solo-3-r"    "61-0406-11-RC2"
+build_mfg "g2h-solo-3f"     "51-0406-411-RC2"
+build_mfg "g2h-solo-4"      "51-0406-201-RC2"
+build_mfg "g2h-solo-4f"     "51-0406-401-RC2"
+build_mfg "g2h-solo-6"      "999-00100-RC2"
+build_mfg "g2h-solo-7f"     "61-0406-361ES1-RC2"
+build_mfg "g2h-solo-11f"    "51-0406-051-RC2"
+build_mfg "g2h-solo-11f-r"  "61-0406-41-RC2"
+build_mfg "g2h-solo-12f"    "51-0406-042-RC2"
+build_mfg "g2h-solo-13"     "51-0406-181-RC2"
+build_mfg "g2h-solo-13f"    "51-0406-081-RC2"
+build_mfg "g2h-solo-14"     "51-0406-191ES-RC2"
+build_mfg "g2h-solo-14f"    "51-0406-091-RC2"
+build_mfg "g2h-solo-18f"    "51-0406-0D2-RC2"
+build_mfg "g2h-solo-19f"    "51-0406-0C1-RC2"
 
 IMG_BUILT=$(ls -lt *.zip | wc -l)
 

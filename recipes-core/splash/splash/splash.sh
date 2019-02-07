76f92a624fd7432261f309b1f984944dec8e95f2
Steve Sakoman <steve@sakoman.com>
2019-02-07 07:18:15 -1000

    splash: add splash screen init

---
+++ recipes-core/splash/splash/splash.sh
@@ -0,0 +1,34 @@
+#!/bin/sh 
+### BEGIN INIT INFO
+# Provides:             splash
+# Required-Start:
+# Required-Stop:
+# Default-Start:        S
+# Default-Stop:
+### END INIT INFO
+
+SPLASH_BMP=/boot/splash.bmp
+
+if [ ! -e /dev/fb0 ]; then
+    echo "Framebuffer /dev/fb0 not detected"
+    echo "Boot splashscreen disabled"
+    exit 0;
+fi
+
+if [ ! -e $SPLASH_BMP ]; then
+    echo "Splash bitmap file not detected"
+    echo "Boot splashscreen disabled"
+    exit 0;
+fi
+
+read CMDLINE < /proc/cmdline
+for x in $CMDLINE; do
+        case $x in
+        splash=false)
+		echo "Boot splashscreen disabled" 
+		exit 0;
+                ;;
+        esac
+done
+
+/usr/bin/fbi -vt 1 -d /dev/fb0 --noverbose $SPLASH_BMP
--- recipes-core/splash/splash_0.01.bb
+++ recipes-core/splash/splash_0.01.bb
@@ -1,14 +1,24 @@
 DESCRIPTION = "Splash screen customization"
 LICENSE = "CLOSED"
 
-SRC_URI = "file://splash.bmp \
-"
+RDEPENDS_${PN} = "fbida"
 
-S = "${WORKDIR}/git"
+SRC_URI = "file://splash.bmp \
+	   file://splash.sh \
+"
 
 do_install() {
     install -d ${D}/boot/
     install -m 0755 ${WORKDIR}/splash.bmp ${D}/boot/
+
+    install -d ${D}${sysconfdir}/init.d
+    install -m 0755    ${WORKDIR}/splash.sh	${D}${sysconfdir}/init.d
 }
 
 FILES_${PN} += "/boot/*.bmp"
+
+inherit update-rc.d
+
+INITSCRIPT_PACKAGES = "${PN}"
+INITSCRIPT_NAME = "splash.sh"
+INITSCRIPT_PARAMS = "start 0 S ."

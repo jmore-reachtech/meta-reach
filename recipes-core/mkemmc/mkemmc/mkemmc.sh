6c4c26e29f4620e5cf37bb1eb7d3dc0b3d414875
Steve Sakoman <steve@sakoman.com>
2020-06-10 06:16:14 -1000

    mkemmc: update script
    1. protected all variables by surrounding with quotes in case file path arguments to script have spaces
    2. added more error checking
    3. added more comments
    4. removed unnecessary dd and sfdisk commands... (that stuff came from the Android Mksdcard.sh script originally, not needed here because there is a partition table embedded in the SD card image)
    5. added a few printfs to let the user know what is going on
    6. added umount of the default VFAT fs in /dev/mmcblk3p1 (mounted at /run/media/mmcblk3p1)
    7. added resize of eMMC partition 4 to fill the available eMMC space... this means /data will be ~ 5GB when booted from eMMC
    8. since parted can not immediately update the kernel with the new partitions (even tried partprobe), borrowed a trick from Raspian and created a semaphore file so the EXT4 filesystem in eMMC partition 4 will automagically be resized at next reboot
    9. added reboot after 5 sec sleep... with a prompt to hit ^C to avoid reboot

--- recipes-core/mkemmc/mkemmc/mkemmc.sh
+++ recipes-core/mkemmc/mkemmc/mkemmc.sh
@@ -1,49 +1,92 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
-	echo ""
-	echo "Usage: $0 <path_to_u-boot_image> <path_to_emmc_sdimg>"
-	exit 1;
+if [ -z "$1" -o -z "$2" ]; then
+	printf "\nUsage: %s <path_to_u-boot_image> <path_to_emmc_sdimg>\n\n" "$0"
+	exit 1
 fi
 
 # path to emmc u-boot
-UBOOT_IMG=$1
+UBOOT_IMG="$1"
+
+if [ ! -e "${UBOOT_IMG}" ]; then
+	printf "\nU-boot image \"%s\" not found\n\n" "${UBOOT_IMG}"
+	exit 1
+fi
 
 # path to emmc sdimg
-EMMC_SDIMG=$2
- 
-# partition size in MB
-BOOT_ROM_SIZE=10
+EMMC_SDIMG="$2"
 
+if [ ! -e "${EMMC_SDIMG}" ]; then
+	printf "\neMMC SD image \"%s\" not found\n\n" "${EMMC_SDIMG}"
+	exit 1
+fi
+ 
 # emmc device
 EMMC_DEV=/dev/mmcblk3
 EMMC_RO_FLAG=/sys/block/mmcblk3boot0/force_ro
 
 # wait for the emmc device node ready
-while [ ! -e ${EMMC_DEV} ]
+while [ ! -e "${EMMC_DEV}" ]
 do
 	sleep 1
-	echo "waiting for ${EMMC_DEV} to appear"
+	printf "waiting for \"%s\" to appear...\n" "${EMMC_DEV}"
 done
 
-# destroy the partition table
-dd if=/dev/zero of=${EMMC_DEV} bs=1024 count=1
+# the eMMC comes from the factory with a single VFAT partition which will be automounted
+# make sure it's not mounted, otherwise the kernel will not see the updated partition
+# table after the image is falshed to the eMMC
 
-# call sfdisk to create new partition table
-sfdisk --force ${EMMC_DEV} << EOF
-${BOOT_ROM_SIZE}M,500M,0c
-600M,,83
-EOF
+EMMC_PART1=/run/media/mmcblk3p1
 
+if [ -e "${EMMC_PART1}" ]; then
+	printf "\nunmounting eMMC default VFAT partition at \"%s\"\n" "${EMMC_PART1}"
+	umount "${EMMC_PART1}"
+fi
+ 
 # write 4 emmc paritions
-dd if=${EMMC_SDIMG} of=${EMMC_DEV}
+#	p1 - VFAT - /uboot
+#	p2 - EXT4 - rootfs A
+#	p3 - EXT4 - rootfs B
+#	p4 - EXT4 - /data
+printf "\nflashing eMMC SD image \"%s\" to \"%s\"\n" "${EMMC_SDIMG}" "${EMMC_DEV}"
+dd if="${EMMC_SDIMG}" of="${EMMC_DEV}"
 
 # write u-boot image (after clearing read only flag for boot0 partition
-echo 0 > ${EMMC_RO_FLAG}
-dd if=${UBOOT_IMG} of=${EMMC_DEV}boot0 bs=512 seek=2
-echo 1 > ${EMMC_RO_FLAG}
+echo 0 > "${EMMC_RO_FLAG}"
+dd if="${UBOOT_IMG}" of="${EMMC_DEV}boot0" bs=512 seek=2
+echo 1 > "${EMMC_RO_FLAG}"
 
 # set emmc to boot from boot0 partition and configure emmc hardware to reflect our connection config
-mmc bootpart enable 1 1 ${EMMC_DEV}
-mmc bootbus set single_backward x1 x4 ${EMMC_DEV}
+printf "\nsetting up eMMC boot\n"
+mmc bootpart enable 1 1 "${EMMC_DEV}"
+mmc bootbus set single_backward x1 x4 "${EMMC_DEV}"
 
+# expand p4 to use the rest of eMMC
+EMMC_PART4="${EMMC_DEV}p4"
+
+printf "\nexpanding eMMC partition 4 (/data)\n"
+parted "${EMMC_DEV}" << EOF
+print
+resizepart 4
+-1s
+print
+EOF
+
+# we have to reboot to see the new partitions... partprobe can't update the kernel
+# even though nothing is mounted from eMMC because something in the kernel is
+# holding a reference to at least one eMMC resource
+
+# this is similar to how Raspian expands its rootfs on first boot
+
+printf "\nsetting up resize of EXT4 filesystem in \"%s\" at next reboot\n" "${EMMC_PART4}"
+touch /.resize_emmc_part4
+
+printf "\nThe system must reboot to use the new eMMC partitions.\n\n"
+
+printf "Rebooting in 5 seconds.. press ^C to cancel\n\n"
+
+sleep 5
+
+sync
+sync
+reboot
